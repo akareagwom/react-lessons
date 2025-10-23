da5489a389f98a67e752235ad8ada5a2185e2223
akareagwom <akareazikang20@gmail.com>
2025-10-23 21:07:42 +0100

    onchange event

--- src/Event-Handlers/Form.jsx
+++ src/Event-Handlers/Form.jsx
@@ -3,18 +3,22 @@ import { useState } from "react";
 function Form() {
   const [name, setName] = useState("");
 
+  const handleNameChange=(e)=>{
+    setName(e.target.value)
+  }
+
   return (
     <div className="bg-fuchsia-950 shadow p-4 flex w-full justify-between h-[70vh] text-white ">
       <div className="w-[40%]">
         <div className="flex justify-between items-baseline w-[100%]">
-          <h1 className="text-2xl">Welcome Collins</h1>
+          <h1 className="text-2xl">Welcome {name}</h1>
           <div className="relative">
             <p>notifications</p>
           </div>
         </div>
       </div>
       <div className="bg-fuchsia-500 rounded flex flex-col text-black gap-4 p-6 w-[50%] ">
-        <input className="bg-white rounded p-2" type="text" />
+        <input value={name} onChange={handleNameChange} className="bg-white rounded p-2" type="text" />
         <input className="bg-white rounded p-2" type="email" name="" id="" />
         <div className="relative">
           <p className="">1</p>
